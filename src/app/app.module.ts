a83b502cb8897268645d521c3ea64663388c9639
Prajakta Joshi <40360231+prajakta02@users.noreply.github.com>
2020-06-12 19:25:34 +0530

    header and footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,18 +2,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import {MatButtonModule, MatCheckboxModule, MatListModule} from '@angular/material';
+import {MatButtonModule, MatCheckboxModule, MatListModule, MatCard, MatCardModule } from '@angular/material';
+import { MatGridListModule } from '@angular/material/grid-list';
+
 import {FlexLayoutModule } from '@angular/flex-layout';
 
-
+import {HeaderComponent} from './header/header.component';
+import {FooterComponent } from './footer/footer.component'
 import { AppComponent } from './app.component';
 import 'hammerjs';
 import { MenuComponent } from './menu/menu.component';
+import { DishdetailComponent } from './dishdetail/dishdetail.component';
+import { DishService } from './services/dish.service';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MenuComponent
+    MenuComponent,
+    DishdetailComponent,
+    HeaderComponent,
+    FooterComponent
+
   ],
   imports: [
     BrowserModule,
@@ -22,10 +31,14 @@ import { MenuComponent } from './menu/menu.component';
     MatToolbarModule,
     MatButtonModule,
     MatCheckboxModule,
-    MatListModule
+    MatListModule,
+    MatGridListModule,
+    MatCardModule
 
   ],
-  providers: [],
+  providers: [
+    DishService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
